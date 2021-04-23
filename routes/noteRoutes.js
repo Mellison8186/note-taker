9a04ddf9e14326feb388effb6f5e7231585498ea
maribels_are_ringing <maribel.duron86@gmail.com>
2021-04-23 12:45:43 -0700

    added POST route

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,16 +1,31 @@
+const fs = require('fs');
 const path = require('path');
+const { request, json } = require('express');
+const express = require('express');
 const router = require('express').Router();
-
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-
-router.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+const { noteTaker } = require('../db/db.json');
+// const { v4: uuidv4 } = require('uuid');
+// uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+  router.get('/api/notes', (req, res) => {
+    res.json(noteTaker);
+  });
+
+  router.post('/api/notes', (req, res) => {
+    console.log(req.body)
+    res.json(req.body);
+  });
+
+  router.delete('/api/notes/:id', (req, res) => {
+    (req.params.id);
+  });
+
+  router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
   module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-// const fs = require('fs');
-// const path = require('path');
+const path = require('path');
 const { request } = require('express');
 const express = require('express');
+const router = require('express').Router();
 // const PORT = process.env.PORT || 3001;
 const app = express();
-const noteRoutes = require('./routes/noteRoutes.js');
+const noteRoutes = require('./routes/noteRoutes');
+// const { v4: uuidv4 } = require('uuid');
+// uuidv4(); // '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+
+app.use(express.static('public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 const { db } = require('./db/db.json');
 
-// app.use(express.static('public'));
-// // parse incoming string or array data
-// app.use(express.urlencoded({ extended: true }));
-// // parse incoming JSON data
-// app.use(express.json());
-
+// Use noteRoutes
 app.use('/', noteRoutes);
 
-app.get('./routes', (req, res) => {
-    res.send();
-});
+// app.get('./routes/noteRoutes', (req, res) => {
+//     res.json(db);
+// });
 
 app.listen(3001, () => {
     console.log(`API server now on Port 3001!`);
