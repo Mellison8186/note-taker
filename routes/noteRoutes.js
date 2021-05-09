819515323c101c5473fafe2ae0d7e7f19241e956
maribels_are_ringing <maribel.duron86@gmail.com>
2021-05-08 18:10:37 -0700

    fixed save and delete methods

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -3,25 +3,37 @@ const path = require('path');
 const { request, json } = require('express');
 const express = require('express');
 const router = require('express').Router();
-const { noteTaker } = require('../db/db.json');
-// const { v4: uuidv4 } = require('uuid');
-// uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const notes = require('../db/db.json');
+const { uuid } = require('uuidv4');
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
   router.get('/api/notes', (req, res) => {
-    res.json(noteTaker);
+    res.json(notes);
   });
 
-  router.post('/api/notes', (req, res) => {
+  router.post('/api/notes', async (req, res) => {
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuid()
+    }
+    notes.push(newNote)
+    await fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes))
     console.log(req.body)
     res.json(req.body);
   });
 
-  router.delete('/api/notes/:id', (req, res) => {
-    res.send(req.params.id);
+  router.delete('/api/notes/:id', async (req, res) => {
+    const notesToKeep = await notes.filter((note) => {
+      console.log(note.id)
+      console.log(req.params.id)
+      return note.id !== req.params.id
+    })
+    await fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notesToKeep))
+    res.send(notesToKeep);
   });
 
   router.get('*', (req, res) => {
